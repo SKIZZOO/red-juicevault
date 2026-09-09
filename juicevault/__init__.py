0757efc680a3991c5a2f9933ce06fb6456103d96
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-09 05:51:00 +0300

    Enable EQ control in JuiceVault panel

--- juicevault/__init__.py
+++ juicevault/__init__.py
@@ -2,10 +2,12 @@ from .juicevault import JuiceVault
 from .juicevault_ui import JuiceVaultUI
 from .api_sources import patch_juicevault_class
 from .ui_patch import patch_ui
+from .eq_ui_patch import patch_eq_controls
 
 
 patch_juicevault_class(JuiceVault)
 patch_ui(JuiceVaultUI)
+patch_eq_controls()
 
 
 async def setup(bot):
