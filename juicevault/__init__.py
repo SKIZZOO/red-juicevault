e237631ceecf933eea01043ff72309c568c57602
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-09 05:17:22 +0300

    Apply polished JuiceVault UI

--- juicevault/__init__.py
+++ juicevault/__init__.py
@@ -1,9 +1,11 @@
 from .juicevault import JuiceVault
 from .juicevault_ui import JuiceVaultUI
 from .api_sources import patch_juicevault_class
+from .ui_patch import patch_ui
 
 
 patch_juicevault_class(JuiceVault)
+patch_ui(JuiceVaultUI)
 
 
 async def setup(bot):
