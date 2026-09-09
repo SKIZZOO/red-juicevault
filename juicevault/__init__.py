e00695cbabc1ebbc49d59b7c498ebc2fbeb2a51e
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-09 06:22:23 +0300

    Install online music search patch

--- juicevault/__init__.py
+++ juicevault/__init__.py
@@ -4,12 +4,14 @@ from .api_sources import patch_juicevault_class
 from .ui_patch import patch_ui
 from .eq_ui_patch import patch_eq_controls
 from .voice_patch import patch_voice_location
+from .external_search_patch import patch_external_search
 
 
 patch_juicevault_class(JuiceVault)
 patch_ui(JuiceVaultUI)
 patch_eq_controls()
 patch_voice_location()
+patch_external_search()
 
 
 async def setup(bot):
