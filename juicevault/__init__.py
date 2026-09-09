f0c65496a49cdd35e0a60e9fddaf74e6736fe5d2
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-09 06:06:50 +0300

    Respect manual voice channel moves

--- juicevault/__init__.py
+++ juicevault/__init__.py
@@ -3,11 +3,13 @@ from .juicevault_ui import JuiceVaultUI
 from .api_sources import patch_juicevault_class
 from .ui_patch import patch_ui
 from .eq_ui_patch import patch_eq_controls
+from .voice_patch import patch_voice_location
 
 
 patch_juicevault_class(JuiceVault)
 patch_ui(JuiceVaultUI)
 patch_eq_controls()
+patch_voice_location()
 
 
 async def setup(bot):
